1475eff37dd1bed29ffeedab53adbdf9fd4430c4
SvdNeo <sanjay.d@neoito.com>
2023-12-22 14:06:07 +0530

    Searching algorithm

--- prototypal_inheritance.js
+++ prototypal_inheritance.js
@@ -88,3 +88,25 @@ let animal1 = {
   
   console.log( rabbit1.jumps ); // undefined
 
+  //task2
+  let head = {
+    glasses: 1
+  };
+  
+  let table = {
+    pen: 3,
+    __proto__:head
+  };
+  
+  let bed = {
+    sheet: 1,
+    pillow: 2,
+    __proto__:table
+  };
+  
+  let pockets = {
+    money: 2000,
+    __proto__:bed
+  };
+  console.log(pockets.glasses)
+
